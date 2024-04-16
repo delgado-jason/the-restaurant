1aaf3684ddbbd8b5405b2be136a581a34061532f
delgado-jason <delgadorjason7@gmail.com>
2024-04-15 22:31:19 -0400

    Build the homepage template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('I\'m from index.js');
+import Hero from '../assets/images/hero.jpeg';
+
+const contentDiv = document.getElementById('content');
+
+const heroImg = new Image();
+heroImg.src = Hero;
+
+contentDiv.appendChild(heroImg);
