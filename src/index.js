ce247ed522cf308245c34a22dea6da2264f3feed
Jason Delgado <delgadorjason7@gmail.com>
2026-02-05 10:42:43 -0500

    refactor(index.js): add click handlers for each button

--- src/index.js
+++ src/index.js
@@ -3,8 +3,37 @@ import renderHome from './modules/renderHome'
 import heroImage from './hero.jpg'
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('The DOM is fully loaded.');
+    console.log('The DOM is fully loaded.')
     renderHome(heroImage)
-});
 
+    // Add event listeners to each button
+    const buttons = document.getElementsByTagName('button')
+    Object.values(buttons).forEach(button => {
+        const text = button.innerText
+        if (text === 'Home') {
+            button.onclick = handleHomeClick
+        }
 
+        if (text === 'Menu') {
+            button.onclick = handleMenuClick
+        }
+
+        if (text === 'About') {
+            button.onclick = handleAboutClick
+        }
+    })
+})
+
+// Click Handlers
+
+const handleHomeClick = () => {
+    console.log('You clicked the "Home" button')
+}
+
+const handleMenuClick = () => {
+    console.log('You clicked the "Menu" button')
+}
+
+const handleAboutClick = () => {
+    console.log('You clicked the "About" button')
+}
