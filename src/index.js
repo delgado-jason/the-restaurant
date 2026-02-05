18973f777bb7b0d8fdf4560203c673d821a253b2
Jason Delgado <delgadorjason7@gmail.com>
2026-02-05 09:12:27 -0500

    refactor(index.js): refactor code to use modules
    The code now uses modules that are called from the 'modules' folder.
    
    This was refactored this way so that I can keep the code isolated
    to each component, allowing for easier debugging.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import './styles.css'
+import renderHome from './modules/renderHome'
 import heroImage from './hero.jpg'
 
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('The DOM is fully loaded.');
-
-    const contentContainer = document.getElementById('content')
-    contentContainer.innerHTML = `
-        <h1>The Restaurant</h1>
-        <img class="hero" src=${heroImage} />
-        <p>We named this restaurant the way our customers' refer to us when they talk about going out for a great dining experience.</p>
-        <p>They say, "Honey, wanna go out to <strong>The Restaurant</strong> tonight?</p>
-    `
+    renderHome(heroImage)
 });
 
 
---
+++ src/modules/renderHome.js
@@ -0,0 +1,13 @@
+
+
+const renderHome = function (img) {
+    const contentContainer = document.getElementById('content')
+    contentContainer.innerHTML = `
+        <h1>The Restaurant</h1>
+        <img class="hero" src=${img} />
+        <p>We named this restaurant the way our customers' refer to us when they talk about going out for a great dining experience.</p>
+        <p>They say, "Honey, wanna go out to <strong>The Restaurant</strong> tonight?</p>
+    `
+}
+
+export default renderHome
