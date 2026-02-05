dd52063d94d284986937f2df2ffb6e53716af4a7
Jason Delgado <delgadorjason7@gmail.com>
2026-02-05 08:35:56 -0500

    chore(homepage): add content to the homepage
    Basic content was added to the homepage. In the future we will
    dynamically render the content instead of it being static.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './styles.css'
+import heroImage from './hero.jpg'
+
+const image = document.createElement('img')
+image.src = heroImage
+image.className = 'hero'
+
+const mainContent = document.getElementById('content')
+mainContent.appendChild(image)
 
 console.log('Bundled successfuly!')
