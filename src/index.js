2685382439fee923e348e678e6567e762a15026b
Jason Delgado <delgadorjason7@gmail.com>
2026-02-05 09:03:09 -0500

    refactor(homepage): refactor homepage to dynamically render
    The homepage now renders dynamically from index.js.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import './styles.css'
 import heroImage from './hero.jpg'
 
-const image = document.createElement('img')
-image.src = heroImage
-image.className = 'hero'
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('The DOM is fully loaded.');
 
-const mainContent = document.getElementById('content')
-mainContent.appendChild(image)
+    const contentContainer = document.getElementById('content')
+    contentContainer.innerHTML = `
+        <h1>The Restaurant</h1>
+        <img class="hero" src=${heroImage} />
+        <p>We named this restaurant the way our customers' refer to us when they talk about going out for a great dining experience.</p>
+        <p>They say, "Honey, wanna go out to <strong>The Restaurant</strong> tonight?</p>
+    `
+});
 
-console.log('Bundled successfuly!')
+
